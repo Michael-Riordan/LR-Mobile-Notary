e4b569abc421956aa4e12931f4ce898b7e4c6026
Michael <michael.riordan81@outlook.com>
2025-10-02 13:20:20 -0700

    troubleshooting

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,7 @@ import ServiceInfo from "./components/ServiceInfo.jsx";
 import ServicesList from "./components/ServicesList.jsx";
 import PricingSection from "./components/Pricing.jsx";
 
-
+/*
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
@@ -19,5 +19,12 @@ export default function Home() {
       </main>
       <Footer />
     </div>
+  );
+}
+*/
+
+export default function Home() {
+  return (
+    <>Hello</>
   );
 }
